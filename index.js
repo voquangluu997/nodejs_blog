3c9b41b431e8f9bfbbe33a77218e81f4f33fe69a
voquangluu997 <voquangluu997@gmail.com>
2021-02-24 12:54:58 +0700

    init commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  var a = 1;
+  var b = 2;
+  var c = a + b;
+  return res.send("Hello World!ss");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
